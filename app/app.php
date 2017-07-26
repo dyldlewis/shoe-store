c30dc693847747b94bd4a1a8612c96c57ae35526
Dylan Lewis <dyldlewis@gmail.com>
2017-07-25 19:22:13 -0700

    create brand creatinga and view routes

--- app/app.php
+++ app/app.php
@@ -17,5 +17,28 @@
         'twig.path' => __DIR__.'/../views'
     ));
 
+    $app->get("/", function() use ($app) {
+        return $app['twig']->render('index.html.twig');
+    });
+
+    $app->get("/brands", function() use ($app) {
+        return $app['twig']->render('brands.html.twig', array('brands' => Brand::getAll()));
+    });
+
+    $app->post("/brands", function() use ($app) {
+        $name = $_POST['name'];
+        $price = $_POST['price'];
+        $brand = new Brand($name, $price);
+        $brand->save();
+        return $app['twig']->render('brands.html.twig', array('brands' => Brand::getAll()));
+    });
+
+    $app->get("/brands/{id}", function($id) use ($app) {
+        $brand = Brand::find($id);
+        return $app['twig']->render('brand.html.twig', array('brand' => $student, 'stores' => $brand->getStores, 'allStores' => Store::getAll()));
+    });
+
+    return $app;
+
 
  ?>
